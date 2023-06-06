850469960ab48f1477006750e4ba6975293570bb
NIKI <74093781+NIKI-weed@users.noreply.github.com>
2023-06-06 16:48:21 +0300

    Add files via upload
    second

---
+++ Main/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MyMath;
+
+namespace Main
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            Console.WriteLine(Algebra.SquareQuadraticEquation(12, 12, 12));
+            Console.WriteLine(Algebra.SquareLinearEquation(12, 12, 12, 12));
+
+            double[] mass = new double[10];
+            for (int i = 0; i < mass.Length; i++)
+                mass[i] = i;
+            Console.WriteLine(Algebra.Summ(mass));
+            Console.WriteLine(Algebra.Maxi(mass));
+            Console.WriteLine(Algebra.Avg(mass));
+
+
+            Console.WriteLine(Geometry.TriangleP(12, 12));
+            Console.WriteLine(Geometry.StraightTriangleP(12, 12));
+            Console.WriteLine(Geometry.RectangleP(12, 12));
+            Console.WriteLine(Geometry.RoundP(12));
+            Console.WriteLine(Geometry.TrapezoidP(12, 12, 12));
+
+            Console.WriteLine(Trigonometry.SinA(12, 12));
+            Console.WriteLine(Trigonometry.CosA(12, 12));
+            Console.WriteLine(Trigonometry.TgA(12, 12));
+            Console.WriteLine(Trigonometry.CtgA(12, 12));
+            Console.WriteLine(Trigonometry.ArcSinA(12, 12));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MyMath/Geometry.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyMath
+{
+
+    public class Algebra
+    {
+        public static double SquareQuadraticEquation(double a, double b, double c)
+        {
+            Console.WriteLine("Корни(-ень) квадратного уравнения");
+            return (b * b) - 4 * a * c;
+        }
+
+        public static double SquareLinearEquation(double D, double a, double b, double x)
+        {
+            Console.WriteLine("Корни линейного уравнения");
+            return D = (a * x) + b;
+            if (x != b / a)
+            {
+                Console.WriteLine($"Один корень + {x}");
+            }
+            else if (x == b / 0)
+            {
+                Console.WriteLine("Нет корней");
+            }
+            else if (x == 0)
+            {
+                Console.WriteLine("Имеет множество решений");
+            }
+            return x = (a * x) + b;
+        }
+
+        public static double Summ(double[] mass)
+        {
+            Console.WriteLine("Сумма ряда цифр");
+            double sum = 0;
+            for (int i = 0; i < mass.Length; i++)
+            {
+                sum += mass[i];
+            }
+            return sum;
+        }
+
+        public static double Maxi(double[] mass)
+        {
+            Console.WriteLine("Максимальное из ряда цифр");
+            double max = 0;
+            for (int i = 0; i < mass.Length; i++)
+            {
+                if (max < mass[i]) 
+                {
+                    max = mass[i];
+                }
+            }
+            return max;
+        }
+
+        public static double Avg(double[] mass)
+        {
+            Console.WriteLine("Cреднее из ряда цифр");
+            double avg = 0;
+            for (int i = 0; i < mass.Length; i++)
+            {
+                avg += mass[i];
+            }
+            return avg/mass.Length;
+        }
+    }
+
+    public class Geometry
+    {
+        public static double TriangleP(double a, double h)
+        {
+            Console.WriteLine("Площадь треугольника");
+            return 0.5 * a * h;
+        }
+
+        public static double StraightTriangleP(double a, double b)
+        {
+            Console.WriteLine("Площадь прямоугольного треугольника");
+            return 0.5 * a * b;
+        }
+
+        public static double RectangleP(double a, double b)
+        {
+            Console.WriteLine("Площадь прямоугольника");
+            return a * b;
+        }
+
+        public static double RoundP(double r)
+        {
+            Console.WriteLine("Площадь круга");
+            return Math.PI * Math.Pow(r, 2);
+        }
+
+        public static double TrapezoidP(double a, double b, double h)
+        {
+            Console.WriteLine("Площадь трапеции");
+            return (a * b) / 2 * h;
+        }
+    }
+
+    public class Trigonometry
+    {
+        public static double SinA(double a, double b)
+        {
+            Console.WriteLine("Sin α");
+            return a / b;
+        }
+
+        public static double CosA(double a, double b)
+        {
+            Console.WriteLine("Cos α");
+            return a / b;
+        }
+
+        public static double TgA(double a, double b)
+        {
+            Console.WriteLine("Tg α");
+            return a / b;
+        }
+
+        public static double CtgA(double a, double b)
+        {
+            Console.WriteLine("Ctg α");
+            return a / b;
+        }
+
+        public static double ArcSinA(double a, double b)
+        {
+            Console.WriteLine("ArcSin α");
+            return Math.Abs(a / b);
+            
+        }
+    }
+}
